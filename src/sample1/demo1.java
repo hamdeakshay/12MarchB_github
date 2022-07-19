69d2be844a62a9fa053fff041a2bc877b7389223
ajitshirsat <ajit.vikramrao.shirsat@gmail.com>
2022-07-19 13:52:49 +0530

    [Sanjay]: added new project

---
+++ src/sample1/demo1.java
@@ -0,0 +1,17 @@
+package sample1;
+
+public class demo1
+{
+	public static void main(String[] args) {
+		
+		System.out.println("Hi");
+		
+		System.out.println("Hi");
+		
+		System.out.println("Hello");
+		
+		System.out.println("Hello");
+				
+	}
+
+}
